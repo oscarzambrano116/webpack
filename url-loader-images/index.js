dfb1c7aaf5d057d560cc18e37d838bc8b1ddf4a3
Oscar Zambrano <oscarz.developer@gmail.com>
2018-02-18 15:13:16 -0400

    Adding url loader

---
+++ url-loader-images/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+import { firstMessage, delayedMessage } from './message.js';
+import platziImg from './platzi.png';
+document.write(firstMessage);
+delayedMessage();
+const img = document.createElement('img');
+img.setAttribute('src', platziImg);
+img.setAttribute('width', 50);
+img.setAttribute('height', 50);
+document.body.append(img);
+console.log('Webpack con webpack.config...');
