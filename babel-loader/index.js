a5c8cc7d8963b469d3d10ee150be9fa6238dcb24
Oscar Zambrano <oscarz.developer@gmail.com>
2018-02-18 14:42:05 -0400

    Adding babel loader

---
+++ babel-loader/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+import { firstMessage } from './message.js';
+document.write(firstMessage);
+console.log('Webpack con webpack.config...');
---
+++ babel-loader/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        }),
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+  ]
+}
