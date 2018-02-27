503e7632a7cb4b8065b73d2a6ba93d43d67e3bf2
Oscar Zambrano <oscarz.developer@gmail.com>
2018-02-27 12:58:35 -0400

    Adding less loader

---
+++ less-loader/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react'],
+          },
+        },
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader',
+      },
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/, // Types of images and fonts
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000, // images size (bytes)
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        }),
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        }),
+      },
+      {
+        test: /\.less/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', {
+            loader: 'less-loader',
+            options: {
+              noIecompat: true,
+            }
+          }]
+        }),
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+  ]
+}
